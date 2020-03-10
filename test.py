0b4680fedc85f11319dd8ceead1b32bb5428c6b8
unknown <andres_dfx@hotmail.com>
2020-03-10 09:41:40 -0500

    Add test freeling

---
+++ test.py
@@ -0,0 +1,10 @@
+import nltk
+from nltk.corpus import cess_esp
+from nltk.corpus import stopwords
+
+nltk.download()
+nltk.corpus.cess_esp.words() #Listado de palabras de Ancora desde nltk
+stopwords.words('spanish') #Stopwords
+nltk.corpus.cess_esp.fileids() #
+palabras=nltk.corpus.cess esp.words('t5-1.tbf')
+palabras[1:20]
